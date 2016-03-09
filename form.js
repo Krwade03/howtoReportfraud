2a97714f697963ae5e69e47c8be00ac0494a806a
Kevin Wade <krwade1988@yahoo.com>
2016-03-08 19:58:30 -0500

    Final project html done, rest remaining

---
+++ form.js
@@ -0,0 +1,71 @@
+
+<link href="https://www.gstatic.com/_/freebird/_/ss/k=freebird.v.-143qunrytor50.L.I11.O/d=1/rs=AMjVe6jf4hbSPj5eYz00ELRPuN78GSjXuw" rel="stylesheet">
+<link href="https://www.gstatic.com/_/freebird/_/ss/k=freebird.v.-143qunrytor50.L.I11.O/d=1/rs=AMjVe6jf4hbSPj5eYz00ELRPuN78GSjXuw" rel="stylesheet">
+<link href="https://www.gstatic.com/_/freebird/_/ss/k=freebird.v.-143qunrytor50.L.I11.O/d=1/rs=AMjVe6jf4hbSPj5eYz00ELRPuN78GSjXuw" rel="stylesheet">
+<link href="https://www.gstatic.com/_/freebird/_/ss/k=freebird.v.-143qunrytor50.L.I11.O/d=1/rs=AMjVe6jf4hbSPj5eYz00ELRPuN78GSjXuw" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style id="WTVccd">.freebirdThemedTab .exportTab .freebirdThemedBadge {background-color: rgba(0, 0, 0, 0.5);}.freebirdThemedTab .exportTab.isSelected 
+.freebirdThemedBadge {background-color: rgb(219, 68, 55);}.freebirdThemedCheckbox.isChecked:not(.isDisabled) {border-color: rgb(219, 68, 55);}.freebirdThemedCheckbox.isCheckedNext > 
+.exportInk, .freebirdThemedCheckbox.isFocused > .exportInk {background-color: rgba(219, 68, 55, 0.15);}.freebirdThemedRadio.isChecked:not(.isDisabled) 
+.exportOuterCircle, .freebirdThemedRadio .exportInnerCircle {border-color: rgb(219, 68, 55);}.freebirdThemedRadio.isCheckedNext > .exportInk, 
+.freebirdThemedRadio.isFocused > .exportInk {background-color: rgba(219, 68, 55, 0.15);}.freebirdThemedTab .exportIndicator {background-color: rgb(219, 68, 55);}.freebirdThemedTab 
+.exportTab {color: rgba(0, 0, 0, 0.5);}.freebirdThemedTab .exportTab.isSelected {color: rgb(219, 68, 55);}.freebirdThemedTab .exportTabPageButton {fill: rgb(219, 68, 55);}.freebirdThemedTab 
+.exportInk {background-image: radial-gradient( circle farthest-side,rgba(219, 68, 55, 0.15),rgba(219, 68, 55, 0.15) 80%,rgba(219, 68, 55, 0) 100% );}.freebirdThemedTextarea 
+.exportFocusUnderline {background-color: rgb(219, 68, 55);}.freebirdThemedTextarea .exportHint {color: rgb(219, 68, 55);}.freebirdThemedTextarea.exportHasError 
+.exportContent.exportUnderline, .freebirdThemedTextarea.exportHasError .exportContent.exportFocusUnderline {background-color: rgb(244, 199, 195);}.quantumWizTextinputPaperinputInput:not([disabled]):focus ~ .quantumWizTextinputPaperinputFloatingLabel.exportLabel, .isFocused > .exportContent >.quantumWizTextinputPapertextareaFloatingLabel.exportLabel, .isFocused.modeDark > .exportContent >.quantumWizTextinputPapertextareaFloatingLabel.exportLabel {color: rgb(219, 68, 55);}.freebirdThemedInput .exportFocusUnderline {background-color: rgb(219, 68, 55);}.freebirdThemedInput .exportHint {color: rgb(219, 68, 55);}.freebirdThemedInput.exportHasError .exportContent.exportUnderline, .freebirdThemedInput.exportHasError .exportContent.exportFocusUnderline {background-color: rgb(244, 199, 195);}.freebirdThemedToggle.isChecked .exportThumb {border-color: rgb(219, 68, 55); color: rgb(255, 255, 255);}.freebirdThemedToggle.isChecked > .exportTrack {border-color: rgb(244, 199, 195);}.freebirdThemedToggle.isCheckedNext > .exportInk, .freebirdThemedToggle.isFocused > .exportInk {background-color: rgba(219, 68, 55, 0.15);}.freebirdThemedToggleSwitch {border-color: rgb(219, 68, 55);}.freebirdThemedToggleSwitch.isChecked {background-color: rgb(219, 68, 55); color: rgb(255, 255, 255);}.freebirdDarkIcon {display: none;}.freebirdThemedText {color: rgb(219, 68, 55);}.accentThemeText {color: rgb(255, 138, 128);}.freebirdHeaderMast {background-color: rgb(219, 68, 55);color: rgb(255, 255, 255);}.freebirdHeaderMastWithOverlay {background-color: rgb(219, 68, 55);color: rgb(255, 255, 255);}.freebirdSolidBackground {background-color: rgb(219, 68, 55); color: rgb(255, 255, 255);}.freebirdSolidColor {color: rgb(219, 68, 55);}.freebirdSolidFill {fill: rgb(219, 68, 55); stroke: rgb(219, 68, 55);}.freebirdLightBackground {background-color: rgb(251, 233, 231);}.freebirdUltraLightBackground {background-color: rgba(251, 233, 231, 0.25);}.freebirdAccentBackground {background-color: rgb(255, 138, 128); color: rgb(0, 0, 0);}.freebirdDisclaimerColor, .freebirdDisclaimerColor a {color: rgb(100, 100, 100);}.exportSplash {background-color: rgb(219, 68, 55);}</style>
+
+<link href="//ssl.gstatic.com/docs/forms/device_home/android_192.png" rel="icon" sizes="192x192">
+
+
+<div class="freebirdFormviewerViewItemsItemItemTitle" dir="auto">
+Which track would you like to enroll for? <span class="freebirdFormviewerViewItemsItemRequiredAsterisk" aria-hidden="true">*</span>
+</div>
+
+<div class="freebirdFormviewerViewItemsItemItemHelpText" id="i.desc.648959660" dir="auto">Choose one
+ (or if you're going to commit to more than one, two is the limit).  Please do not commit to two tracks unless you're certain you
+  have the capacity to complete both (with projects)!  Note the operating system requirements (if applicable). 
+  Note: We're holding off on .NET Development this session, pending the release of Treehouse's .NET Development track 
+  (pending for September 2016 session).</div>
+
+
+<span class="freebirdMaterialCheckboxwithlabelLabelText freebirdFormviewerViewItemsCheckboxLabel" dir="auto">PHP Development
+ (on Thursdays at Code Louisville)</span>
+
+<span class="freebirdMaterialCheckboxwithlabelLabelText freebirdFormviewerViewItemsCheckboxLabel" dir="auto">Ruby on Rails
+ Development (on Tuesdays at Code Louisville)</span>
+<span class="freebirdMaterialCheckboxwithlabelLabelText freebirdFormviewerViewItemsCheckboxLabel" dir="auto">Ruby on Rails Development 
+(on Tuesdays at Code Louisville)</span>
+
+<div class="quantumWizTogglePapercheckboxInnerBox"></div>
+<div class="quantumWizTogglePapercheckboxInnerBox"></div>
+<div class="quantumWizTogglePapercheckboxInnerBox"></div>
+<div class="quantumWizTogglePapercheckboxInnerBox"></div>
+
+
+<div class="freebirdFormviewerViewItemsItemErrorMessage" id="i.err.648959660" role="alert" jsname="XbIQze">
+This is a required question</div>
+
+<div class="freebirdFormviewerViewItemsItemItemTitle" dir="auto">First Name <span class="freebirdFormviewerViewItemsItemRequiredAsterisk" aria-hidden="true">
+*</span></div>
+
+<div class="freebirdFormviewerViewItemsItemItemTitle" dir="auto">Last Name <span class="freebirdFormviewerViewItemsItemRequiredAsterisk" aria-hidden="true">
+*</span></div>
+
+<div class="freebirdFormviewerViewItemsItemItemTitle" dir="auto">Email address <span class="freebirdFormviewerViewItemsItemRequiredAsterisk" aria-hidden="true">
+*</span></div>
+<div class="freebirdFormviewerViewItemsItemItemTitle" dir="auto">Email address <span class="freebirdFormviewerViewItemsItemRequiredAsterisk" aria-hidden="true">
+*</span></div>
+
+<div class="freebirdFormviewerViewItemsItemItemHelpText" id="i.desc.1251703570"
+  dir="auto">Double-check your address!</div>
+
+<input tabindex="0" class="quantumWizTextinputPaperinputInput exportInput" aria-describedby="i.desc.1251703570 i.err.1251703570" dir="auto" aria-label="Email address" 
+required="" type="text" value="" jsname="YPqjbf" data-initial-value=""
+ data-initial-dir="auto" autocomplete="off">
+
+ <span class="quantumWizButtonPaperbuttonLabel">Submit</span>
+
+<div class="freebirdFormviewerViewNavigationPasswordWarning">Never submit passwords through Google Forms.</div>
+
+ 
+
